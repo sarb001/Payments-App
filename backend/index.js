d3d95aeb750c2cb76155a71a9bca294880e93bf3
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-18 09:29:34 +0530

    config - routes created

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-import express from 'express';
 
+import express from 'express';
+import Router from './routes/UserRoute';
 const app = express();
 
 const PORT  = 3000;
@@ -7,3 +8,9 @@ const PORT  = 3000;
 app.listen(PORT , () => {
     console.log('Server Running on  PORT',PORT);
 })
+
+app.get('/api/v1',Router);
+
+// signup
+// login
+// update info 
---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const route = express.Router();
+
+route.get('/signup' , () => {  });
+
+route.get('/login' , () => {  });
+
+route.get('/updateinfo' , () => {  })
+
+export default route;
+
