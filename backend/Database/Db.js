7e9797dcd40ae0d2af606649ddcb3df989bd82d4
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-27 07:21:50 +0530

    config - variables stored

--- backend/Database/Db.js
+++ backend/Database/Db.js
@@ -4,13 +4,24 @@ import mongoose from "mongoose";
 // using simple javascript for connection
 //  without async/await 
 
-const DatabaseConn = () => {
+// const DatabaseConn = () => {
 
-      mongoose.connect('mongodb://localhost:27017/payments-app')
-    .then(() => {
-        console.log('mongodb connected')
-    }).catch((err) => console.log('connection err -',err));
+//       mongoose.connect('mongodb://localhost:27017/payments-app')
+//     .then(() => {
+//         console.log('mongodb connected')
+//     }).catch((err) => console.log('connection err -',err));
     
+// }
+
+
+const  DatabaseConn = async() => {
+    try {     
+        await mongoose.connect(process.env.DB_NAME);
+        console.log(' Mongodb Connected ');
+    } catch (error) {
+        console.log('error =',error);
+        process.exit(1);
+    }
 }
 
 export default DatabaseConn;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
 import Router from './routes/UserRoute.js';
+import dotenv from 'dotenv';
 import DatabaseConn from './Database/Db.js';
 const app = express();
 
 app.use(express.json());
+
+dotenv.config();
 
 const PORT  = 3000;
 DatabaseConn();
