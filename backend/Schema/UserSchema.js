421d12ba15516b00f11d55a9f3d9841db4eb578a
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-29 06:58:21 +0530

    feat - user schema created

---
+++ backend/Schema/UserSchema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    username : {
+         type : String,
+         unique  : true,
+         required : true
+    },
+    firstname : {
+        type :String ,   
+         unique  : true,
+         required : true
+    },
+    lastname : {
+         type :String ,   
+         unique  : true,
+         required : true
+    },
+    password : {
+         type :String ,   
+         unique  : true,
+         required : true
+    }
+})
