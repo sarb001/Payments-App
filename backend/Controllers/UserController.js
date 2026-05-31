524cddc8902296fef9a3d04b323a61156938120a
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-31 16:38:49 +0530

    feat -password hashing

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,26 +1,39 @@
 import express from 'express';
 import { User } from '../Schema/UserSchema.js';
+import bcrypt from 'bcrypt';
 
 const app = express();
 
  export const  SignupHandler = async(req,res) => {
 
-     const { username , firstname , lastname , password }   = req.body;
-     console.log('basic body -',username , firstname , lastname , password);
+    try {
+        
+        const { username , firstname , lastname , password }   = req.body;
+        console.log('basic body -',username , firstname , lastname , password);
 
-     if(!username ||  !firstname || !lastname || !password){
-         return res.json({
-            message : " Enter all input Fields "
+        if(!username ||  !firstname || !lastname || !password){
+            return res.json({
+                message : " Enter all input Fields "
+            })
+        }
+
+        const PasswordEnc = await bcrypt.hash(password,10);
+        console.log('Pass enc -',PasswordEnc);
+
+    //   const  user = await User.create({ username,firstname,lastname,password : PasswordEnc });
+
+    //  console.log('new user is  1 -',user);
+
+        return res.status(200).json({
+            message : "User Created"
+        }); 
+       }
+
+    catch (error) {
+         return res.status(500).json({
+            message : "Signup Failed"
          })
-     }
-
-     const  user = await User.create({ username,firstname,lastname,password });
-
-     console.log('new user is  1 -',user);
-
-    return res.status(200).json({
-        message : "User Created"
-    })
+    }
 };
 
  export const  LoginHandler = async(req,res) => {
