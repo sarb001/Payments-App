043e1f39a2ba1ac6a51a05cda14646909b43486f
iamsarb001 <sarbbsandhu555@gmail.com>
2026-06-27 23:32:44 +0530

    style - signup cards designed

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@
 export const Dashboard = () => {
     return (
         <>
-         <div className = "text-2xl bg-red-200"> 
+         <div className = "text-2xl bg-red-700"> 
             Inside the Dashboard  
         </div>
         </>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,8 +2,48 @@
 
 export const Signup = () => {
     return (
-        <>
-           <div> Signup component </div> 
-        </>
+        <div className="bg-slate-300 h-[100vh] w-full flex justify-center ">
+
+            <div className=" bg-white p-4 m-4 rounded-lg ">
+
+                <div className="text-3xl font-bold flex justify-center"> Sign Up </div> 
+                <div className="py-4 "> Enter your information to create an account </div>
+
+                    <div>
+                        <div>
+                            <div> First Name </div>
+                            <input type = "text"  value = ""  placeholder="John" 
+                              className="border-2 border-black focus:border-0 my-2 rounded-md w-full"
+                            />
+                        </div>
+                        <div className="py-4">
+                            <div> Last Name </div>
+                            <input type = "text"  value = ""  placeholder="Doe" 
+                             className="border-2 border-black focus:border-0 my-2 rounded-md w-full"
+                            />
+                        </div>
+                        <div>
+                            <div> Email </div>
+                            <input type = "text"  value = ""  placeholder="johndoe@example.com" 
+                             className="border-2 border-black focus:border-0 my-2 rounded-md w-full"
+                            />
+                        </div>
+                        <div className="py-4">
+                            <div> Password </div>
+                            <input type = "text"  value = ""  placeholder = "************"
+                            className="border-2 border-black focus:border-0 my-2 rounded-md w-full"
+                             />
+                        </div>
+                        <div className="flex w-full justify-center">
+                            <button className="text-white bg-black px-32 py-[5px] rounded-md  "> Sign Up </button>
+                        </div>
+                        <div className="py-2">
+                            <span> Already have an account? Login </span>
+                        </div>
+                    </div>
+
+            </div>
+
+        </div>
     )
 }
